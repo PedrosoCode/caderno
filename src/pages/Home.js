52d5f4885d17240d7b7350c95a42c6960ce14b63
PedrosoCode <Gabrielpedrofc@gmail.com>
2023-11-09 22:29:30 -0300

    Formatação sendo preparada para receber conteúdo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,88 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import MainNavbar from '../components/MainNavbar';
-import '../bulma/bulma/css/bulma.css'
-import '../index.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import MainNavbar from "../components/MainNavbar";
+import "../bulma/bulma/css/bulma.css";
+import "../index.css";
 
 const Home = () => {
   return (
-    <div className=''>
-        <MainNavbar></MainNavbar>
-      <h1>Bem-vindo à Página Inicial!</h1>
+    <div classNameName="">
+      <MainNavbar></MainNavbar>
+      <section className="section is-medium">
+        <h1 className="title">Bem vindo ao meu caderno de estudos</h1>
+        <h2 className="subtitle">
+          Um local para eu consultar minhas anotações, e demonstrar meus
+          interesses
+        </h2>
+      </section>
       <p>
         Esta é a sua página inicial. Para ver o roadmap, clique no link abaixo:
       </p>
       <Link to="/roadmap">Ver Roadmap</Link>
       {/* Outros conteúdos da página inicial podem ser adicionados aqui */}
+
+      <div className="box mybox">
+        <div className="columns">
+          <div className="column is-8 maincol commomMargin">
+            is-two-thirds <br></br> teste{" "}
+          </div>
+          <div className="column is-3 maincol commomMargin">Auto</div>
+        </div>
+      </div>
+      <div className="mybox">
+      <aside class="menu">
+        <p class="menu-label">Linguagens de programação</p>
+        <ul class="menu-list">
+          <li>
+            <a>Dashboard</a>
+          </li>
+          <li>
+            <a>Customers</a>
+          </li>
+        </ul>
+        <p class="menu-label">Administration</p>
+        <ul class="menu-list">
+          <li>
+            <a>Team Settings</a>
+          </li>
+          <li>
+            <a class="is-active">Manage Your Team</a>
+            <ul>
+              <li>
+                <a>Members</a>
+              </li>
+              <li>
+                <a>Plugins</a>
+              </li>
+              <li>
+                <a>Add a member</a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a>Invitations</a>
+          </li>
+          <li>
+            <a>Cloud Storage Environment Settings</a>
+          </li>
+          <li>
+            <a>Authentication</a>
+          </li>
+        </ul>
+        <p class="menu-label">Transactions</p>
+        <ul class="menu-list">
+          <li>
+            <a>Payments</a>
+          </li>
+          <li>
+            <a>Transfers</a>
+          </li>
+          <li>
+            <a>Balance</a>
+          </li>
+        </ul>
+      </aside>
+      </div>
     </div>
   );
 };
--- src/pages/Roadmap.js
+++ src/pages/Roadmap.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import MainNavbar from "../components/MainNavbar";
+import Footer from "../components/Footer";
+import "../bulma/bulma/css/bulma.css";
+import "../index.css";
 
 const Roadmap = () => {
   return (
     <div>
-      <h2>Roadmap</h2>
-      <p>
-        Aqui você pode adicionar informações sobre o roadmap do seu projeto.
-      </p>
+      <MainNavbar></MainNavbar>
+      <section className="section is-medium">
+        <h1 className="title">Roadmaps</h1>
+        <h2 className="subtitle">
+          tópicos, milestones e interesses <br></br>
+          This a way, to Keep track of my Progress <br></br> <br></br>
+          <Link to="/">-Retornar para a Home</Link>
+        </h2>
+        <Footer></Footer>
+      </section>
     </div>
   );
 };
