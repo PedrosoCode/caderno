128953f0e767a16e0a4c0e876354b33bb1543105
PedrosoCode <Gabrielpedrofc@gmail.com>
2023-11-10 07:45:50 -0300

    criação de texto, e formatação

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -30,59 +30,46 @@ const Home = () => {
         </div>
       </div>
       <div className="mybox">
-      <aside class="menu">
-        <p class="menu-label">Linguagens de programação</p>
-        <ul class="menu-list">
-          <li>
-            <a>Dashboard</a>
-          </li>
-          <li>
-            <a>Customers</a>
-          </li>
-        </ul>
-        <p class="menu-label">Administration</p>
-        <ul class="menu-list">
-          <li>
-            <a>Team Settings</a>
-          </li>
-          <li>
-            <a class="is-active">Manage Your Team</a>
-            <ul>
-              <li>
-                <a>Members</a>
-              </li>
-              <li>
-                <a>Plugins</a>
-              </li>
-              <li>
-                <a>Add a member</a>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <a>Invitations</a>
-          </li>
-          <li>
-            <a>Cloud Storage Environment Settings</a>
-          </li>
-          <li>
-            <a>Authentication</a>
-          </li>
-        </ul>
-        <p class="menu-label">Transactions</p>
-        <ul class="menu-list">
-          <li>
-            <a>Payments</a>
-          </li>
-          <li>
-            <a>Transfers</a>
-          </li>
-          <li>
-            <a>Balance</a>
-          </li>
-        </ul>
-      </aside>
+        <aside class="menu">
+          <p class="menu-label">Linguagens de programação</p>
+          <ul class="menu-list">
+            <li>
+              <a>WEB (HTML, CSS e JS)</a>
+            </li>
+            <li>
+              <a>Python</a>
+            </li>
+          </ul>
+          <p class="menu-label">Frameworks</p>
+        </aside>
       </div>
+      <div className="box mybox">
+      <div class="content is-large">
+        <h1>Hello World</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+          accumsan, metus ultrices eleifend gravida, nulla nunc varius lectus,
+          nec rutrum justo nibh eu lectus. Ut vulputate semper dui. Fusce erat
+          odio, sollicitudin vel erat vel, interdum mattis neque.
+        </p>
+        <h2>Second level</h2>
+        <p>
+          Curabitur accumsan turpis pharetra <strong>augue tincidunt</strong>{" "}
+          blandit. Quisque condimentum maximus mi, sit amet commodo arcu rutrum
+          id. Proin pretium urna vel cursus venenatis. Suspendisse potenti.
+          Etiam mattis sem rhoncus lacus dapibus facilisis. Donec at dignissim
+          dui. Ut et neque nisl.
+        </p>
+        <ul>
+          <li>In fermentum leo eu lectus mollis, quis dictum mi aliquet.</li>
+          <li>Morbi eu nulla lobortis, lobortis est in, fringilla felis.</li>
+          <li>
+            Aliquam nec felis in sapien venenatis viverra fermentum nec lectus.
+          </li>
+          <li>Ut non enim metus.</li>
+        </ul>
+      </div>
+    </div>
     </div>
   );
 };
--- src/pages/Roadmap.js
+++ src/pages/Roadmap.js
@@ -18,6 +18,33 @@ const Roadmap = () => {
         </h2>
         <Footer></Footer>
       </section>
+      <div className="box mybox">
+        <div class="content is-large">
+          <h1>Hello World</h1>
+          <p>
+            Muitas são as disponíveis técnologias no mercado, portanto fica
+            difícil me organizar para testar algumas marcar progresso, sendo
+            assim bolei a ídeia de montar um roadmap inspirado <a></a> por este site aqui  
+          </p>
+          <h2>Second level</h2>
+          <p>
+            Curabitur accumsan turpis pharetra <strong>augue tincidunt</strong>{" "}
+            blandit. Quisque condimentum maximus mi, sit amet commodo arcu
+            rutrum id. Proin pretium urna vel cursus venenatis. Suspendisse
+            potenti. Etiam mattis sem rhoncus lacus dapibus facilisis. Donec at
+            dignissim dui. Ut et neque nisl.
+          </p>
+          <ul>
+            <li>In fermentum leo eu lectus mollis, quis dictum mi aliquet.</li>
+            <li>Morbi eu nulla lobortis, lobortis est in, fringilla felis.</li>
+            <li>
+              Aliquam nec felis in sapien venenatis viverra fermentum nec
+              lectus.
+            </li>
+            <li>Ut non enim metus.</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
